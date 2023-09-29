36f1e770d5460671fe067a5db3d9e69a45dfe4dc
tobionesies <d2ba5@pm.me>
2023-09-29 09:52:44 +0200

    add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "g3-imageshare",
+//   storageBucket: "YOUR_STORAGE_BUCKET",
+//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+//   appId: "YOUR_APP_ID",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
