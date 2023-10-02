7114c3558a189be9c903fa6f2d4d9b51ff45ecd9
tobionesies <d2ba5@pm.me>
2023-10-02 13:50:48 +0200

    configured firebase to host our frontend

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,17 @@
-import firebase from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_AUTH_DOMAIN",
+  apiKey: "AIzaSyAOLZtdaCkQ35Ntchx7FItdqIgVZV2addI",
+  authDomain: "g3-imageshare.firebaseapp.com",
   projectId: "g3-imageshare",
-//   storageBucket: "YOUR_STORAGE_BUCKET",
-//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//   appId: "YOUR_APP_ID",
+  storageBucket: "g3-imageshare.appspot.com",
+  messagingSenderId: "678418147318",
+  appId: "1:678418147318:web:6808b1d1f125c622ecc9d7"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
