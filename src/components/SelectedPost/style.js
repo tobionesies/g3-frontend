330fc87896f62e37e62b26ade61f2f37fa270cc3
tobionesies <d2ba5@pm.me>
2023-10-03 14:07:26 +0200

    add SelectedPost component

---
+++ src/components/SelectedPost/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    width: 50%;
+    z-index: 9;
+    position: absolute;
+    background-color: bisque;
+`
+
+export const LeftCol = styled.div`
+    flex: 1;
+    background-color: tomato;
+    
+    `
+
+export const RightCol = styled.div`
+    flex: 1;
+    background-color: beige;
+
+`
