92a40311fddf6cf927438a27f53b5b575682aa86
Nadya Khan <nadyakhan24@live.com>
2023-10-03 15:21:43 +0200

    added a path to settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import FeedPage from './view/FeedPage'
 import LandingPage from './view/LandingPage'
 import { AuthProvider } from './contexts/Context';
-import Settings from './components/Settings/settings';
+import Settingspage from './components/Settings/settings';
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/view/FeedPage" element={<FeedPage />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route path="/settings" element={<Settingspage />} />
       </Routes>/
     </AuthProvider>
     </>
