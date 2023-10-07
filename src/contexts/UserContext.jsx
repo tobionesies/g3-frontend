7a1a3ad2aa26f78b5291f27e955e5b5bb3cc1a1e
Radovan <radosce0@gmail.com>
2023-10-07 20:21:46 +0200

    fixed export

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,24 +1,28 @@
+import React, { createContext, useState, useEffect } from "react";
+ 
+export const UserContext = createContext();
+ 
 export const UserProvider = ({ children }) => {
-    const [singleUser, setSingleUser] = useState({});
-    const getUserById = async (userId) => {
-      try {
-        const response = await fetch(
-          "https://api-4uzdo5gwpq-uc.a.run.app/api/user/" + userId,
-          {
-            method: "GET",
-          }
-        );
-        const json = await response.json();
-        setSingleUser(json)
-      } catch (error) {
-        console.log(error);
-      }
-    };
-   
-    return (
-      <UserContext.Provider value={{ singleUser, getUserById }}>
-        {children}
-      </UserContext.Provider>
-    );
+  const [singleUser, setSingleUser] = useState({});
+  const getUserById = async (userId) => {
+    try {
+      const response = await fetch(
+        "https://api-4uzdo5gwpq-uc.a.run.app/api/user/" + userId,
+        {
+          method: "GET",
+        }
+      );
+      const json = await response.json();
+      setSingleUser(json)
+    } catch (error) {
+      console.log(error);
+    }
   };
-   
+ 
+  return (
+    <UserContext.Provider value={{ singleUser, getUserById }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+ 
