ad435119d10b0b36b4035e2964eb911d962d617c
tobionesies <d2ba5@pm.me>
2023-10-10 15:09:44 +0200

    style textField

--- src/components/TextField/style.js
+++ src/components/TextField/style.js
@@ -2,13 +2,21 @@ import styled from 'styled-components'
  
 export const TextFieldDiv = styled.div`
     display: flex;
-    width: 350px;
-    height: 60px;
+    /* width: 350px;
+    height: 60px; */
 `
 export const TextInput = styled.input`
-    width: 350px;
+    /* width: 350px; */
     height: 25px;
-    padding: 10px;
-    border: solid black 1px;
-    border-radius: 10px;
+    /* padding: 10px; */
+    /* border: solid black 1px; */
+    /* border-radius: 10px;  */
+    background-color: #f0f2f5;
+    width: 340px;
+    padding: 5px;
+    border-radius: 15px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border-style: none;
+    border: 1px solid grey;
 `
