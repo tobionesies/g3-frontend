b86f3fe084c1770cfcadeae398888897bd0d77b2
Nadya Khan <nadyakhan24@live.com>
2023-10-12 13:03:05 +0200

    fixed myImages

--- src/view/MyImages.jsx
+++ src/view/MyImages.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import LeftMenu from '../components/LeftMenu/LeftMenu'
-import RightMenu from '../components/RightMenu/RightMenu'
-import Content from '../components/Content/Content'
+import React from "react";
+import ImagePost from "../components/ImagePost/ImagePost";
+import { useAppContext } from "../auth";
+import { useQuery } from "react-query";
 
-const MyImages = () => {
+const MyImages = ({ userId }) => {
+  const { state } = useAppContext();
+
+  const getPosts = async () => {
+    const response = await fetch(
+      "https://api-4uzdo5gwpq-uc.a.run.app/api/post",
+      {
+        method: "GET",
+        headers: {
+          authorization: `${state.credential.credentials.token}`,
+        },
+      }
+    );
+    return await response.json();
+  };
+
+  const { data: allPosts, error, isLoading } = useQuery("posts", getPosts);
+
+  if (isLoading)
+    return (
+      <div>
+        
+        Loading...
+      </div>
+    );
+  if (error) return <div>Error: {error.message}</div>;
+
+  // Check if allPosts is defined before filtering it
+  const userPosts = allPosts.filter((post) => post.user_id === state.credential.credentials.userId);
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'stretch', margin: '0', padding: '0' }}>
-     {/* <Content /> */}
-    <h1>Your content Here!</h1>
-  </div>
-  )
-}
+    <div>
+      {userPosts.map((post, index) => (
+        <ImagePost key={index} post={post} />
+      ))}
+    </div>
+  );
+};
 
-export default MyImages
+export default MyImages;
